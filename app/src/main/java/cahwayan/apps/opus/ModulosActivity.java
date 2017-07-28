f0ee49dc5b30233f44a12c80df426a039436be55
Caíque Andrade <cahwayan@gmail.com>
2017-07-28 17:57:14 -0300

    initial commit

---
+++ app/src/main/java/cahwayan/apps/opus/ModulosActivity.java
@@ -0,0 +1,13 @@
+package cahwayan.apps.opus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModulosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulos);
+    }
+}
